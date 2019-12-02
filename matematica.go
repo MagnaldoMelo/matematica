f4007aa69cbca9ae441170b65ac6481aefc095ca
MagnaldoMelo <magnaldo.melo@gmail.com>
2019-12-01 21:40:16 -0300

    Create matematica.go

---
+++ matematica.go
@@ -0,0 +1,17 @@
+package matematica
+
+func Soma(a, b float64) (float64) {
+    return a+b
+}
+
+func Subtrai(a, b float64) (float64) {
+    return a-b
+}
+
+func Multiplica(a, b float64) (float64) {
+    return a*b
+}
+
+func Divide(a, b float64) (float64) {
+    return a/b
+}
